c089a1f924167d2cfc4cae2126c718a4e231890c
Fulvius <fulvius@btcbanco.com.br>
2016-09-27 17:32:12 -0300

    .

--- www/js/model/player.js
+++ www/js/model/player.js
@@ -1,32 +1,33 @@
+var palyer = {
+    pontosDeVida: 0,
+    danoTotal: 0,
+    danoUltimoAtaque: 0,
+    iniciativa: 0,
+    pontosDeExperiencia: 0,
+    nivel: function(){
+        return palyer.nivelPorExperiencia( pontosDeExperiencia ).nivel;
+    },
+    proficiencia: function(){
+        return palyer.nivelPorExperiencia( pontosDeExperiencia ).proficiencia;
+    },
+    nivelPorExperiencia: function ( experiencia = 0 ){
+         var obj = null;
+         $.each( experienciaNivelProficiencia, function( key, value ) {
+           // use i as an array index
+           console.log('Key: '+ key + ' and Value: ' + value);
+           if (experiencia >= key){
+             obj = value
+           }
+           console.log('Nível: ' + obj.nivel + ' Proficiencia: ' + obj.proficiencia);
+         });
+         return obj;
+     }
+};
+
 $.getScript( "js/model/domains.js" )
       .done(function( script, textStatus ) {
         console.log( "Script loaded: " + textStatus );
-
-            var palyer = {
-                pontosDeVida: 0,
-                danoTotal: 0,
-                danoUltimoAtaque: 0,
-                iniciativa: 0,
-                pontosDeExperiencia: 0,
-                nivel: function(){
-                    return palyer.nivelPorExperiencia( pontosDeExperiencia ).nivel;
-                },
-                proficiencia: function(){
-                    return palyer.nivelPorExperiencia( pontosDeExperiencia ).proficiencia;
-                },
-                nivelPorExperiencia: function ( experiencia = 0 ){
-                     var obj = null;
-                     $.each( experienciaNivelProficiencia, function( key, value ) {
-                       // use i as an array index
-                       console.log('Key: '+ key + ' and Value: ' + value);
-                       if (experiencia >= key){
-                         obj = value
-                       }
-                       console.log('Nível: ' + obj.nivel + ' Proficiencia: ' + obj.proficiencia);
-                     });
-                     return obj;
-                 }
-            };
+            palyer.nivelPorExperiencia(0);
       })
       .fail(function( jqxhr, settings, exception ) {
         console.log( "Script load fail: " + exception );
